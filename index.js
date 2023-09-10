4325dcf4aa34703ff2d9bc899291945d60ca1834
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-09-10 18:23:45 -0400

    added function to get rid of duplicates in array

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+//sort array
 
+function sortArray(array) {
+    let sorted = array.sort(function (a, b) {  return a - b;  });
+       return sorted
+ }
+ 
+function removeDuplicates(sortedArray) {
+
+ for (let i = 0; i < sortedArray.length; i++) {
+     if (i > 0) {
+         if (sortedArray[i] == sortedArray[i -1])
+         sortedArray.splice([i - 1], 1)
+     }
+
+ }
+    return sortedArray
+   }
 
 
 // JavaScript program to print BST in given range
@@ -20,6 +37,7 @@ var root = null;
 from a sorted array */
 function sortedArrayToBST(arr, start, end)
 {
+   
 	/* Base Case */
 	if (start > end)
 	{
@@ -63,19 +81,22 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
-var n = arr.length;
+let arr2 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+
+function buildTree(arr) {
+sortArray(arr)
+removeDuplicates(arr)
+console.log(arr)
+let n = arr.length;
 root = sortedArrayToBST(arr, 0, n - 1);
 console.log("Preorder traversal of constructed BST<br>");
 preOrder(root);
 
 prettyPrint(root)
+}
 
-let arr2 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-
-    function sortArray(array) {
-       let sorted = array.sort(function (a, b) {  return a - b;  });
-          return sorted
-    }
+buildTree(arr2)
 
 
-console.log(sortArray(arr2))
+
+

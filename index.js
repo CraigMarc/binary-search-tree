8ff5d213b0b83d99eb30f9d6c25bde9c3a172435
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-09-10 17:31:52 -0400

    added binary search tree constructors and viewing functions

---
+++ index.js
@@ -0,0 +1,75 @@
+
+
+
+// JavaScript program to print BST in given range
+
+// A binary tree node
+class Node
+{
+	constructor(d)
+	{
+		this.data = d;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+var root = null;
+
+/* A function that constructs Balanced Binary Search Tree
+from a sorted array */
+function sortedArrayToBST(arr, start, end)
+{
+	/* Base Case */
+	if (start > end)
+	{
+		return null;
+	}
+	/* Get the middle element and make it root */
+	var mid = parseInt((start + end) / 2);
+	var node = new Node(arr[mid]);
+	/* Recursively construct the left subtree and make it
+	left child of root */
+	node.left = sortedArrayToBST(arr, start, mid - 1);
+	/* Recursively construct the right subtree and make it
+	right child of root */
+	node.right = sortedArrayToBST(arr, mid + 1, end);
+	return node;
+}
+/* A utility function to print preorder traversal of BST */
+function preOrder(node)
+{
+	if (node == null)
+	{
+		return;
+	}
+	console.log(node.data + " ");
+	preOrder(node.left);
+	preOrder(node.right);
+}
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+var n = arr.length;
+root = sortedArrayToBST(arr, 0, n - 1);
+console.log("Preorder traversal of constructed BST<br>");
+preOrder(root);
+
+console.log(prettyPrint(root))
+
+
+    
+

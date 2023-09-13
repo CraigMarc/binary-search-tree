26966d7c8797e4ae62f472b481a17eacc6974b03
CraigMarc <120748724+CraigMarc@users.noreply.github.com>
2023-09-13 16:33:42 -0400

    added find delete and depth functions

--- index.js
+++ index.js
@@ -54,6 +54,8 @@ function sortedArrayToBST(arr, start, end)
 	node.right = sortedArrayToBST(arr, mid + 1, end);
 	return node;
 }
+
+
 /* A utility function to print preorder traversal of BST */
 function preOrder(node)
 {
@@ -66,6 +68,7 @@ function preOrder(node)
 	preOrder(node.right);
 }
 
+//function to print tree
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
     return;
@@ -83,6 +86,9 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18];
 let arr2 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
+
+
+
 function buildTree(arr) {
 sortArray(arr)
 removeDuplicates(arr)
@@ -97,6 +103,68 @@ prettyPrint(root)
 
 buildTree(arr2)
 
+function insert(data, node = this.root) {
+    if (node == null) {
+      node = new Node(data);
+      return node;
+    }
 
+    if (data < node.data) node.left = this.insert(data, node.left);
+    else if (data > node.data) node.right = this.insert(data, node.right);
+    return node;
+  }
 
+  insert(1050)
+  insert (15252562566)
 
+  function del(data, node = root) {
+    if (node == null) return node;
+
+    if (data < node.data) node.left = del(data, node.left);
+    else if (data > node.data) node.right = del(data, node.right);
+    else {
+      // node with only one child or no child
+      if (node.left == null) return node.right;
+      if (node.right == null) return node.left;
+
+      // node with two children
+      node.data = this.minValue(node.right);
+      node.right = this.delete(node.data, node.right);
+    }
+    return node;
+  }
+
+  function minValue(node) {
+    let minv = node.data;
+    while (node.left != null) {
+      minv = node.left.data;
+      node = node.left;
+    }
+    return minv;
+  }
+
+ 
+
+//del(1050)
+prettyPrint(root)
+console.log(minValue(root))
+
+function find(value, root = this.root) {
+	// Return root if null or matches value
+	if (root === null || root.data === value) {
+	return root;
+	}
+
+	// Access root's children if value not found; 
+	if (value < root.data) {
+	return this.find(value, root.left);
+	}
+	return this.find(value, root.right); 
+  }
+
+function depth(data, node = root) {
+    if (node.data === data.data) return 0;
+    if (data.data < node.data) return this.depth(data, node.left) + 1;
+    if (data.data > node.data) return this.depth(data, node.right) + 1;
+  }
+  console.log(depth(find(7)));
